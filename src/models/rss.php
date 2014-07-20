4bf85f3b3e08441e7bdb16355d342f5b32f75f97
clemh78 <clement@hemidy.fr>
2014-07-20 17:28:52 +0200

    Problème avec les flux rss de réglé.

--- src/models/rss.php
+++ src/models/rss.php
@@ -19,11 +19,8 @@ class Rss extends Common{
 		
 		if($url){
 		
-			$fluxrss=simplexml_load_file($url);
+			$fluxrss=simplexml_load_file($url, 'SimpleXMLElement', LIBXML_NOCDATA);
 			
-			/*echo '<pre>';
-			print_r($fluxrss);
-			echo "</pre>";*/
 			
 			foreach($fluxrss->channel->item as $item){
 	            $sql = "SELECT * FROM cacheRss WHERE url = '".addslashes($item->link)."';";
@@ -34,10 +31,8 @@ class Rss extends Common{
 		            $id = $rslt['id'];
 		            $nameFile = $rslt['nameFile'];
 	            }else{
-		            $h = get_headers($item->link);
-		            $e = explode("\"", $h[3]);
-		            $e2 = explode("\"", $e[1]);
-		            $nameFile = $e2[0];
+			    $e = explode("/", $this->get_final_url($item->link));
+		            $nameFile = $e[(count($e)-1)];
 		            
 		            if($nameFile){
 		           	 	$sqlI = "INSERT INTO cacheRss VALUE('', '".addslashes($item->link)."', '".addslashes($nameFile)."');";
@@ -75,6 +70,82 @@ class Rss extends Common{
 	        }
 		}
 	}
+
+	/**
+	 * get_redirect_url()
+	 * Gets the address that the provided URL redirects to,
+	 * or FALSE if there's no redirect. 
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	function get_redirect_url($url){
+	    $redirect_url = null; 
+
+	    $url_parts = @parse_url($url);
+	    if (!$url_parts) return false;
+	    if (!isset($url_parts['host'])) return false; //can't process relative URLs
+	    if (!isset($url_parts['path'])) $url_parts['path'] = '/';
+
+	    $sock = fsockopen($url_parts['host'], (isset($url_parts['port']) ? (int)$url_parts['port'] : 80), $errno, $errstr, 30);
+	    if (!$sock) return false;
+
+	    $request = "HEAD " . $url_parts['path'] . (isset($url_parts['query']) ? '?'.$url_parts['query'] : '') . " HTTP/1.1\r\n"; 
+	    $request .= 'Host: ' . $url_parts['host'] . "\r\n"; 
+	    $request .= "Connection: Close\r\n\r\n"; 
+	    fwrite($sock, $request);
+	    $response = '';
+	    while(!feof($sock)) $response .= fread($sock, 8192);
+	    fclose($sock);
+
+	    if (preg_match('/^Location: (.+?)$/m', $response, $matches)){
+	        if ( substr($matches[1], 0, 1) == "/" )
+	            return $url_parts['scheme'] . "://" . $url_parts['host'] . trim($matches[1]);
+	        else
+	            return trim($matches[1]);
+
+	    } else {
+	        return false;
+	    }
+
+	}
+
+	/**
+	 * get_all_redirects()
+	 * Follows and collects all redirects, in order, for the given URL. 
+	 *
+	 * @param string $url
+	 * @return array
+	 */
+	function get_all_redirects($url){
+	    $redirects = array();
+	    while ($newurl = $this->get_redirect_url($url)){
+	        if (in_array($newurl, $redirects)){
+	            break;
+	        }
+	        $redirects[] = $newurl;
+	        $url = $newurl;
+	    }
+	    return $redirects;
+	}
+
+	/**
+	 * get_final_url()
+	 * Gets the address that the URL ultimately leads to. 
+	 * Returns $url itself if it isn't a redirect.
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	function get_final_url($url){
+	    $redirects = $this->get_all_redirects($url);
+	    if (count($redirects)>0){
+	        return array_pop($redirects);
+	    } else {
+	        return $url;
+	    }
+	}
+
 }
 
 ?>
