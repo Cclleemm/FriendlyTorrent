3ad1adf5357b430a1fe89bfa1bf3a52dd10f8e45
Clément Raussin <clement@raussin.com>
2015-08-24 17:07:20 +0200

    Oups, I forget a "s"

--- src/core/languages/fr.php
+++ src/core/languages/fr.php
@@ -2,7 +2,7 @@
 define("LANG_TITLE", "Friendly Torrent");
 define("LANG_ERROR", "Erreur");
 define("LANG_ERROR_DISCONNECTED", "Vous avez été déconnecté !");
-define("LANG_ERROR_DELETEFORBIDDEN", "Attention petit malin, supprimer des fichier de votre boxe !");
+define("LANG_ERROR_DELETEFORBIDDEN", "Attention petit malin, supprimez des fichiers de votre box !");
 define("LANG_ERROR_CANTDELETE", "Suppression impossible");
 define("LANG_TITLE", "Friendly Torrent");
 define("LANG_DOWNLOADS", "Torrents");
